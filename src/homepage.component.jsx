d4afd6cf02e19fd7b266925ce924e18bd18392f3
jlienard <jefferson.lienard@gmail.com>
2020-04-28 15:48:05 +0200

    files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>CASQUETTES</h1>
+          <span className='subtitle'>ACHETER MAINTENANT</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>VESTES</h1>
+          <span className='subtitle'>ACHETER MAINTENANT</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>BASKETS</h1>
+          <span className='subtitle'>ACHETER MAINTENANT</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>FEMMES</h1>
+          <span className='subtitle'>ACHETER MAINTENANT</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HOMMES</h1>
+          <span className='subtitle'>ACHETER MAINTENANT</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
